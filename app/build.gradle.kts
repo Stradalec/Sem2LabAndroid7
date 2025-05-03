2383e0889047ffbf78a603e7c7db38d9eea6971f
Stradalec <pochtatipo6@gmail.com>
2025-05-03 11:56:18 +0700

    changed SDK version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.sem2labandroid7"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.sem2labandroid7"
