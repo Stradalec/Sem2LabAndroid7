b6c3b2447e60401d40c742743b152360b79755ad
Stradalec <pochtatipo6@gmail.com>
2025-05-04 08:48:15 +0700

    added first version of this completely not working and really bad mapkit

--- app/src/main/java/com/example/sem2labandroid7/MainActivity.kt
+++ app/src/main/java/com/example/sem2labandroid7/MainActivity.kt
@@ -1,20 +1,47 @@
 package com.example.sem2labandroid7
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
+import com.yandex.mapkit.mapview.MapView
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        initializeMapKit()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        mapView = findViewById(R.id.mapview)
+        mapView.getMap().move(
+            CameraPosition(
+                Point(55.354993, 86.085805), 15.0f, 0.0f, 0.0f
+            )
+        )
+
+    }
+    private fun initializeMapKit() {
+        try {
+            MapKitFactory.setApiKey(getString(R.string.mapkit_api_key))
+            MapKitFactory.initialize(this)
+        } catch (e: Exception) {
+            Log.e("MAPKIT", "Ошибка инициализации", e)
+            Toast.makeText(this, "Ошибка карт: ${e.message}", Toast.LENGTH_LONG).show()
         }
+    }
+    override fun onStart() {
+        super.onStart()
+        MapKitFactory.getInstance().onStart()
+        mapView.onStart()
+    }
+
+    override fun onStop() {
+        mapView.onStop()
+        MapKitFactory.getInstance().onStop()
+        super.onStop()
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,6 +19,7 @@ dependencyResolutionManagement {
     }
 }
 
+
 rootProject.name = "Sem2LabAndroid7"
 include(":app")
  
