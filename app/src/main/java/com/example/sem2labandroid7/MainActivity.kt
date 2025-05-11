2b84f8b871d994ae415cf193841ddd79ff01714c
Stradalec <pochtatipo6@gmail.com>
2025-05-11 14:52:38 +0700

    Project clear to transfer to google maps

--- app/src/main/java/com/example/sem2labandroid7/MainActivity.kt
+++ app/src/main/java/com/example/sem2labandroid7/MainActivity.kt
@@ -4,17 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.yandex.mapkit.MapKitFactory
-import com.yandex.mapkit.ScreenPoint
-import com.yandex.mapkit.directions.DirectionsFactory
-import com.yandex.mapkit.directions.driving.DrivingRouterType
-import com.yandex.mapkit.geometry.Point
-import com.yandex.mapkit.map.CameraListener
-import com.yandex.mapkit.map.CameraPosition
-import com.yandex.mapkit.map.InputListener
-import com.yandex.mapkit.map.MapObjectTapListener
-import com.yandex.mapkit.mapview.MapView
-import com.yandex.runtime.image.ImageProvider
 
 
 class MainActivity : AppCompatActivity() {
